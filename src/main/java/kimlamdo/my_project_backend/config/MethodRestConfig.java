b1228d334f3409baa2e7b5220c827a9475e37ed9
LowkeyCoder2408 <21h1120042@ut.edu.vn>
2024-02-26 22:35:27 +0700

    update project

--- src/main/java/kimlamdo/my_project_backend/config/MethodRestConfig.java
+++ src/main/java/kimlamdo/my_project_backend/config/MethodRestConfig.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.Type;
 import kimlamdo.my_project_backend.entity.Brand;
 import kimlamdo.my_project_backend.entity.Category;
+import kimlamdo.my_project_backend.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -13,19 +14,34 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class MethodRestConfig implements RepositoryRestConfigurer {
-    private String url = "http://localhost:8080";
+    private String url = "http://localhost:3000";
 
     @Autowired
     private EntityManager entityManager;
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new));
+        // config.exposeIdsFor(Category.class);
+
+        // CORS configuration
+        cors.addMapping("/**")
+                .allowedOrigins(url)
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+
         HttpMethod[] disabledMethods = {
                 HttpMethod.POST,
                 HttpMethod.PUT,
                 HttpMethod.DELETE,
                 HttpMethod.PATCH,
         };
+        disableHttpMethods(Category.class, config, disabledMethods);
+
+        // Chặn các method DELETE
+        HttpMethod[] deleteMethod = {
+                HttpMethod.DELETE
+        };
+        disableHttpMethods(User.class, config,deleteMethod );
 
         // Allow ID while return JSON
 //        config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new));
