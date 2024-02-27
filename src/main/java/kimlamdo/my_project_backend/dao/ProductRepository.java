db20ec099c2402a063de5af01c5e54c7587d7d97
LowkeyCoder2408 <21h1120042@ut.edu.vn>
2024-02-28 01:15:46 +0700

    update project

--- src/main/java/kimlamdo/my_project_backend/dao/ProductRepository.java
+++ src/main/java/kimlamdo/my_project_backend/dao/ProductRepository.java
@@ -2,11 +2,18 @@ package kimlamdo.my_project_backend.dao;
 
 import kimlamdo.my_project_backend.entity.Order;
 import kimlamdo.my_project_backend.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestResource(path = "product")
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Page<Product> findByNameContaining(@RequestParam("productName") String productName, Pageable pageable);
 
+    Page<Product> findByCategory_Id(@RequestParam("categoryId") int categoryId, Pageable pageable);
+
+    Page<Product> findByNameContainingAndCategory_Id(@RequestParam("productName") String productName, @RequestParam("categoryId") int categoryId, Pageable pageable);
 }
