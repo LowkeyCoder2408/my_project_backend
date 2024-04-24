034746bdb575a931890a0bdb17cdbd17e605b8bd
LowkeyCoder2408 <21h1120042@ut.edu.vn>
2024-04-24 20:13:16 +0700

    update project

--- src/main/java/kimlamdo/my_project_backend/controller/VNPayController.java
+++ src/main/java/kimlamdo/my_project_backend/controller/VNPayController.java
@@ -4,7 +4,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kimlamdo.my_project_backend.config.VNPayConfig;
-import kimlamdo.my_project_backend.dto.VNPayRestDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
