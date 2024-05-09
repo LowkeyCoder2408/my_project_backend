70f3e35e2347ed2e684ba800dcd41c0a32d284bb
LowkeyCoder2408 <21h1120042@ut.edu.vn>
2024-05-09 23:39:13 +0700

    update project

--- src/main/java/kimlamdo/my_project_backend/dao/FavoriteProductRepository.java
+++ src/main/java/kimlamdo/my_project_backend/dao/FavoriteProductRepository.java
@@ -3,8 +3,11 @@ package kimlamdo.my_project_backend.dao;
 import kimlamdo.my_project_backend.entity.Customer;
 import kimlamdo.my_project_backend.entity.FavoriteProduct;
 import kimlamdo.my_project_backend.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,4 +16,6 @@ public interface FavoriteProductRepository extends JpaRepository<FavoriteProduct
     public FavoriteProduct findFavoriteProductByProductAndCustomer(Product product, Customer customer);
 
     public List<FavoriteProduct> findFavoriteProductsByCustomer(Customer customer);
+
+    Page<FavoriteProduct> findByCustomer_Id(@RequestParam("customerId") int customerId, Pageable pageable);
 }
